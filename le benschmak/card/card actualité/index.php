2fe7d9c60cfce3a21f769da612dfad0bb90ad248
jordanbrel87 <jordantalla43@gmail.com>
2024-05-03 10:39:05 +0200

    mettre a jour sur benchmark

---
+++ le benschmak/card/card actualité/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="././styles/index.css">
+</head>
+<body>
+    <div class="blog-section">
+        <div class="section-content blog">
+            <div class="title">
+                <h2>blog & news</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="cards">
+                <div class="card">
+                    <div class="image-section">
+                        <img src="././images/biche.jpeg" alt="biche">
+                    </div>
+                    <div class="article">
+                        <h4>title one</h4>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                             Iure tempore corrupti maxime eius fugit sint doloremque, aliquam sapiente nam impedit eligendi obcaecati earum, 
+                            odit ad doloribus officiis commodi laudantium sit?</p>
+                    </div>
+                    <div class="blog-view">
+                        <a href="" class="button">Read More</a>
+                    </div>
+                    <div class="posted-date">
+                        <p>posted on jan 30 2021</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="image-section">
+                        <img src="././images/poisson.jpg" alt="poisson">
+                    </div>
+                    <div class="article">
+                        <h4>title one</h4>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                             Iure tempore corrupti maxime eius fugit sint doloremque, aliquam sapiente nam impedit eligendi obcaecati earum, 
+                            odit ad doloribus officiis commodi laudantium sit?</p>
+                    </div>
+                    <div class="blog-view">
+                        <a href="" class="button">Read More</a>
+                    </div>
+                    <div class="posted-date">
+                        <p>posted on jan 30 2021</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="image-section">
+                        <img src="././images/1.jpg" alt="1">
+                    </div>
+                    <div class="article">
+                        <h4>title one</h4>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                             Iure tempore corrupti maxime eius fugit sint doloremque, aliquam sapiente nam impedit eligendi obcaecati earum, 
+                            odit ad doloribus officiis commodi laudantium sit?</p>
+                    </div>
+                    <div class="blog-view">
+                        <a href="" class="button">Read More</a>
+                    </div>
+                    <div class="posted-date">
+                        <p>posted on jan 30 2021</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="image-section">
+                        <img src="././images/2.jpg" alt="2">
+                    </div>
+                    <div class="article">
+                        <h4>title one</h4>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                             Iure tempore corrupti maxime eius fugit sint doloremque, aliquam sapiente nam impedit eligendi obcaecati earum, 
+                            odit ad doloribus officiis commodi laudantium sit?</p>
+                    </div>
+                    <div class="blog-view">
+                        <a href="" class="button">Read More</a>
+                    </div>
+                    <div class="posted-date">
+                        <p>posted on jan 30 2021</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="image-section">
+                        <img src="././images/lièvre.jpg" alt="lièvre">
+                    </div>
+                    <div class="article">
+                        <h4>title one</h4>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                             Iure tempore corrupti maxime eius fugit sint doloremque, aliquam sapiente nam impedit eligendi obcaecati earum, 
+                            odit ad doloribus officiis commodi laudantium sit?</p>
+                    </div>
+                    <div class="blog-view">
+                        <a href="" class="button">Read More</a>
+                    </div>
+                    <div class="posted-date">
+                        <p>posted on jan 30 2021</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
